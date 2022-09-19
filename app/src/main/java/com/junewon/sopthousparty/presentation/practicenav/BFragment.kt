0e3b4b88dcab1d2fc653f5341097f7d7abff5864
murjune <sis9221302@gmail.com>
2022-09-19 22:39:26 +0900

    [feat] BFragment 로직 구현

--- app/src/main/java/com/junewon/sopthousparty/presentation/practicenav/BFragment.kt
+++ app/src/main/java/com/junewon/sopthousparty/presentation/practicenav/BFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import com.junewon.sopthousparty.R
+import com.junewon.sopthousparty.data.model.MokdongData
 import com.junewon.sopthousparty.databinding.FragmentBBinding
 import com.junewon.sopthousparty.util.binding.BindingFragment
 
@@ -14,8 +15,17 @@ class BFragment : BindingFragment<FragmentBBinding>(R.layout.fragment_b) {
     }
 
     private fun initClickEvent() {
-        binding.btnNavigate.setOnClickListener {
-            val action = BFragmentDirections.actionBFragmentToCFragment()
+        binding.btnMokdong.setOnClickListener {
+            val action = BFragmentDirections.actionFragmentBToSuccessFragment(
+                MokdongData(
+                    "https://user-images.githubusercontent.com/87055456/191024885-a14f5deb-7f85-4bf8-96a4-21c2fe9e4c04.png",
+                    "목동"
+                )
+            )
+            findNavController().navigate(action)
+        }
+        binding.btnSindorim.setOnClickListener {
+            val action = BFragmentDirections.actionFragmentBToFailFragment()
             findNavController().navigate(action)
         }
     }
