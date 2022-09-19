fd5255352a184b8c09c394c138fdb4d52daaa9d2
murjune <sis9221302@gmail.com>
2022-09-19 22:39:41 +0900

    [feat] SuccessFragment 로직 구현완

---
+++ app/src/main/java/com/junewon/sopthousparty/presentation/practicenav/SuccessFragment.kt
@@ -0,0 +1,24 @@
+package com.junewon.sopthousparty.presentation.practicenav
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.navArgs
+import com.junewon.sopthousparty.R
+import com.junewon.sopthousparty.databinding.FragmentSuccessBinding
+import com.junewon.sopthousparty.util.binding.BindingFragment
+import timber.log.Timber
+
+class SuccessFragment : BindingFragment<FragmentSuccessBinding>(R.layout.fragment_success) {
+    private val args by navArgs<SuccessFragmentArgs>()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.data = args.mokdongData
+        setClickEvent()
+    }
+
+    private fun setClickEvent() {
+        binding.btnBack.setOnClickListener {
+            activity?.onBackPressed() ?: Timber.e("SuccessFragment not attached to an activity.")
+        }
+    }
+}
