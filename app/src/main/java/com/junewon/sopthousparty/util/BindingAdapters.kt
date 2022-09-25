c452c26575a55393aec5b107ef4d64aa38dc2eb5
murjune <sis9221302@gmail.com>
2022-09-25 22:15:43 +0900

    [fix] 코드리뷰 반영

--- app/src/main/java/com/junewon/sopthousparty/util/BindingAdapters.kt
+++ app/src/main/java/com/junewon/sopthousparty/util/BindingAdapters.kt
@@ -3,9 +3,12 @@ package com.junewon.sopthousparty.util
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.load
+import com.junewon.sopthousparty.R
 
 @BindingAdapter("app:imageUrl")
 fun ImageView.setImageUrl(imageUrl: String?) {
-    if (imageUrl == null || imageUrl == "") return
-    load(imageUrl)
+    load(imageUrl) {
+        placeholder(R.drawable.ic_launcher_foreground)
+        error(R.drawable.ic_error_24)
+    }
 }
